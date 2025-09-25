016ca5894635e85905d10c572494465518d1b226
Tania Tudorache <tudorache@users.noreply.github.com>
2025-09-25 11:21:36 +0300

    Added fix to handle multiple linearization specifications on a linearization view, in order to address who-icatx/icatx-project#293.

--- src/main/java/org/who/owl/export/LinearizationViewMap.java
+++ src/main/java/org/who/owl/export/LinearizationViewMap.java
@@ -1,32 +1,70 @@
 package org.who.owl.export;
 
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Map;
+import java.util.Set;
 
 public class LinearizationViewMap {
 
+	public static final String LINEARIZATION_VIEW_MMS = "http://id.who.int/icd/release/11/mms";
+	public static final String LINEARIZATION_VIEW_PCL = "http://id.who.int/icd/release/11/pcl";
+	public static final String LINEARIZATION_VIEW_PCH = "http://id.who.int/icd/release/11/pch";
+	public static final String LINEARIZATION_VIEW_DER = "http://id.who.int/icd/release/11/der";
+	public static final String LINEARIZATION_VIEW_MNH = "http://id.who.int/icd/release/11/mnh";
+	public static final String LINEARIZATION_VIEW_PED = "http://id.who.int/icd/release/11/ped";
+	public static final String LINEARIZATION_VIEW_OCU = "http://id.who.int/icd/release/11/ocu";
+	public static final String LINEARIZATION_VIEW_RAR = "http://id.who.int/icd/release/11/rar";
+	public static final String LINEARIZATION_VIEW_NER = "http://id.who.int/icd/release/11/ner";
+	public static final String LINEARIZATION_VIEW_MUS = "http://id.who.int/icd/release/11/mus";
+	public static final String LINEARIZATION_VIEW_OPH = "http://id.who.int/icd/release/11/oph";
+	public static final String LINEARIZATION_VIEW_ENV = "http://id.who.int/icd/release/11/env";
+	public static final String LINEARIZATION_VIEW_ICD_O = "http://id.who.int/icd/release/11/icd-o";
+	public static final String LINEARIZATION_VIEW_RESEARCH = "http://id.who.int/icd/release/11/research";
+	public static final String LINEARIZATION_VIEW_ICF = "http://id.who.int/icd/release/11/icf";
+	public static final String LINEARIZATION_VIEW_ICHI = "http://id.who.int/icd/release/11/ichi";
+
+	public static final Set<String> TELESCOPIC_LINEARIZATIONS = new HashSet<String>() {{
+		add(LINEARIZATION_VIEW_PCL);
+		add(LINEARIZATION_VIEW_PCH);
+		add(LINEARIZATION_VIEW_DER);
+		add(LINEARIZATION_VIEW_MNH);
+		add(LINEARIZATION_VIEW_PED);
+		add(LINEARIZATION_VIEW_OCU);
+		add(LINEARIZATION_VIEW_RAR);
+		add(LINEARIZATION_VIEW_NER);
+		add(LINEARIZATION_VIEW_MUS);
+		add(LINEARIZATION_VIEW_OPH);
+		add(LINEARIZATION_VIEW_ENV);
+		add(LINEARIZATION_VIEW_RESEARCH);
+	}};
+	
 	public static final Map<String, String> oldLinViewToNewLinViewMap = new HashMap<String, String>() {{
 		
-		put("http://who.int/icd#Morbidity", "http://id.who.int/icd/release/11/mms"); //01
-		put("http://who.int/icd#Primary_Care_Low_RS", "http://id.who.int/icd/release/11/pcl"); //03
-		put("http://who.int/icd#Primary_Care_High_RS", "http://id.who.int/icd/release/11/pch"); //80.2
-		put("http://who.int/icd#Specialty_Adaptation_Dermatology", "http://id.who.int/icd/release/11/der"); //06
-		put("http://who.int/icd#Speciality_Adaptation_Mental_Health","http://id.who.int/icd/release/11/mnh"); //05
-		put("http://who.int/icd#Specialty_Adaptation_Paediatrics","http://id.who.int/icd/release/11/ped"); //09
-		put("http://who.int/icd#Specialty_Adaptation_Occupational_Health","http://id.who.int/icd/release/11/ocu"); //10
-		put("http://who.int/icd#Specialty_Adaptation_Rare_Diseases","http://id.who.int/icd/release/11/rar"); //12
-		put("http://who.int/icd#Specialty_Adaptation_Neurology","http://id.who.int/icd/release/11/ner"); //08
-		put("http://who.int/icd#Specialty_Adaptation_Musculoskeletal","http://id.who.int/icd/release/11/mus"); //07
-		put("http://who.int/icd#Specialty_Adaptation_Ophthalmology","http://id.who.int/icd/release/11/oph"); //13
-		put("http://who.int/icd#Specialty_Adaptation_Environmental_Health", "http://id.who.int/icd/release/11/env"); //11
-		put("http://who.int/icd#ICD-O", "http://id.who.int/icd/release/11/icd-o"); //25
-		put("http://who.int/icd#Research", "http://id.who.int/icd/release/11/research"); //04
-		put("http://who.int/icd#ICFLinearizationView", "http://id.who.int/icd/release/11/icf"); //02.02
-		put("http://who.int/icd#ICHILinearizationView", "http://id.who.int/icd/release/11/ichi"); //02.01
+		put("http://who.int/icd#Morbidity", LINEARIZATION_VIEW_MMS); //01
+		put("http://who.int/icd#Primary_Care_Low_RS", LINEARIZATION_VIEW_PCL); //03
+		put("http://who.int/icd#Primary_Care_High_RS", LINEARIZATION_VIEW_PCH); //80.2
+		put("http://who.int/icd#Specialty_Adaptation_Dermatology", LINEARIZATION_VIEW_DER); //06
+		put("http://who.int/icd#Speciality_Adaptation_Mental_Health", LINEARIZATION_VIEW_MNH); //05
+		put("http://who.int/icd#Specialty_Adaptation_Paediatrics", LINEARIZATION_VIEW_PED); //09
+		put("http://who.int/icd#Specialty_Adaptation_Occupational_Health", LINEARIZATION_VIEW_OCU); //10
+		put("http://who.int/icd#Specialty_Adaptation_Rare_Diseases", LINEARIZATION_VIEW_RAR); //12
+		put("http://who.int/icd#Specialty_Adaptation_Neurology", LINEARIZATION_VIEW_NER); //08
+		put("http://who.int/icd#Specialty_Adaptation_Musculoskeletal", LINEARIZATION_VIEW_MUS); //07
+		put("http://who.int/icd#Specialty_Adaptation_Ophthalmology", LINEARIZATION_VIEW_OPH); //13
+		put("http://who.int/icd#Specialty_Adaptation_Environmental_Health", LINEARIZATION_VIEW_ENV); //11
+		put("http://who.int/icd#ICD-O", LINEARIZATION_VIEW_ICD_O); //25
+		put("http://who.int/icd#Research", LINEARIZATION_VIEW_RESEARCH); //04
+		put("http://who.int/icd#ICFLinearizationView", LINEARIZATION_VIEW_ICF); //02.02
+		put("http://who.int/icd#ICHILinearizationView", LINEARIZATION_VIEW_ICHI); //02.01
 	}};
 	
 	
 	public static String getNewLinearizationViewName(String oldLinViewName) {
 		return oldLinViewToNewLinViewMap.get(oldLinViewName);
+	}
+	
+	public static boolean isTelescopicLinearization(String linView) {
+		return TELESCOPIC_LINEARIZATIONS.contains(linView);
 	}
 }
--- src/main/java/org/who/owl/export/LinearziationExporter.java
+++ src/main/java/org/who/owl/export/LinearziationExporter.java
@@ -5,6 +5,7 @@ import java.util.Collection;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Optional;
 
 import org.apache.log4j.Logger;
 import org.json.simple.JSONObject;
@@ -20,6 +21,15 @@ public class LinearziationExporter {
 	private final static Logger log = Logger.getLogger(LinearziationExporter.class);
 
 	public static final String WHOFIC_LINEARIZATION_SPECIFICATIONS_JSON_KEY = "whoficEntityLinearizationSpecification";
+
+	private static final String LINEARIZATION_VIEW_JSON_KEY = "linearizationView";
+	private static final String IS_INCLUDED_IN_LINEARIZATION_JSON_KEY = "isIncludedInLinearization";
+	private static final String IS_GROUPING_JSON_KEY = "isGrouping";
+	private static final String IS_AUXILIARY_AXIS_CHILD_JSON_KEY = "isAuxiliaryAxisChild";
+	private static final String LINEARIZATION_PARENT_JSON_KEY = "linearizationParent";
+	private static final String CODING_NOTE_JSON_KEY = "codingNote";
+
+	private static final String UNKNOWN = "unknown";
 
 	private ICDContentModel cm;
 	private RDFSNamedClass sourceCls;
@@ -57,11 +67,127 @@ public class LinearziationExporter {
 		for (RDFResource linSpec : linSpecs) {
 			Map<String, String> linMap = exportLinearization(linSpec);
 			if (linMap != null) {
-				linSpecList.add(linMap);
+				Optional<Map<String, String>> altLinMap = getDuplicateLinMap(linSpecList, linMap);
+				if (altLinMap.isPresent()) {
+					updateExistingLinSpec(targetCls, altLinMap.get(), linMap);
+				}
+				else {
+					linSpecList.add(linMap);
+				}
 			}
 		}
 
 		return linSpecList;
+	}
+
+	private Optional<Map<String, String>> getDuplicateLinMap(List<Map<String, String>> linSpecList, Map<String, String> linMap) {
+		String linViewName = linMap.get(LINEARIZATION_VIEW_JSON_KEY);
+		
+        Optional<Map<String, String>> result = linSpecList.stream()
+                .filter(m -> linViewName.equals(m.get(LINEARIZATION_VIEW_JSON_KEY)))
+                .findFirst();
+        
+		return result;
+	}
+
+	private void updateExistingLinSpec(OWLClass targetCls, Map<String, String> firstLinMap, Map<String, String> secondLinMap) {
+		boolean isTelescopic = LinearizationViewMap.isTelescopicLinearization(firstLinMap.get(LINEARIZATION_VIEW_JSON_KEY));
+		
+		String isPartOf1 = firstLinMap.get(IS_INCLUDED_IN_LINEARIZATION_JSON_KEY);
+		String isPartOf2 = secondLinMap.get(IS_INCLUDED_IN_LINEARIZATION_JSON_KEY);
+//		if (! isTelescopic) {
+			if (UNKNOWN.equals(isPartOf1)) {
+				firstLinMap.put(IS_INCLUDED_IN_LINEARIZATION_JSON_KEY, isPartOf2);
+			}
+			else if (UNKNOWN.equals(isPartOf2)) {
+				//do nothing (i.e. keep isPartOf1)
+			}
+			else if (! isPartOf1.equals(isPartOf2)) {
+				log.warn("Conflicting values (" + isPartOf1 + "/" + isPartOf2 + ") for " + IS_INCLUDED_IN_LINEARIZATION_JSON_KEY + " for linearization view " +
+						firstLinMap.get(LINEARIZATION_VIEW_JSON_KEY) + " on entity " + targetCls);
+			}
+//		}
+//		else {
+//			
+//		}
+			
+		String isGrouping1 = firstLinMap.get(IS_GROUPING_JSON_KEY);
+		String isGrouping2 = secondLinMap.get(IS_GROUPING_JSON_KEY);
+//		if (! isTelescopic) {
+			if (UNKNOWN.equals(isGrouping1)) {
+				firstLinMap.put(IS_GROUPING_JSON_KEY, isGrouping2);
+			}
+			else if (UNKNOWN.equals(isGrouping2)) {
+				//do nothing (i.e. keep isGrouping1)
+			}
+			else if (! isGrouping1.equals(isGrouping2)) {
+				log.warn("Conflicting values (" + isGrouping1 + "/" + isGrouping2 + ") for " + IS_GROUPING_JSON_KEY + " for linearization view " +
+						firstLinMap.get(LINEARIZATION_VIEW_JSON_KEY) + " on entity " + targetCls);
+			}
+//		}
+//		else {
+//			
+//		}
+			
+		String isAuxCh1 = firstLinMap.get(IS_AUXILIARY_AXIS_CHILD_JSON_KEY);
+		String isAuxCh2 = secondLinMap.get(IS_AUXILIARY_AXIS_CHILD_JSON_KEY);
+//		if (! isTelescopic) {
+			if (UNKNOWN.equals(isAuxCh1)) {
+				firstLinMap.put(IS_AUXILIARY_AXIS_CHILD_JSON_KEY, isAuxCh2);
+			}
+			else if (UNKNOWN.equals(isAuxCh2)) {
+				//do nothing (i.e. keep isAuxCh1)
+			}
+			else if (! isAuxCh1.equals(isAuxCh2)) {
+				log.warn("Conflicting values (" + isAuxCh1 + "/" + isAuxCh2 + ") for " + IS_AUXILIARY_AXIS_CHILD_JSON_KEY + " for linearization view " +
+						firstLinMap.get(LINEARIZATION_VIEW_JSON_KEY) + " on entity " + targetCls);
+			}
+//		}
+//		else {
+//			
+//		}
+		
+			
+		String linParent1 = firstLinMap.get(LINEARIZATION_PARENT_JSON_KEY);
+		String linParent2 = secondLinMap.get(LINEARIZATION_PARENT_JSON_KEY);
+		if (! isTelescopic) {
+			if (linParent1 == null && linParent2 != null) {
+				firstLinMap.put(LINEARIZATION_PARENT_JSON_KEY, linParent2);
+			}
+			else if (linParent2 == null) {
+				//do nothing (i.e. keep linParent1)
+			}
+			else if (! linParent1.equals(linParent2)) {
+				log.warn("Conflicting values (" + linParent1 + "/" + linParent2 + ") for " + LINEARIZATION_PARENT_JSON_KEY + " for linearization view " +
+						firstLinMap.get(LINEARIZATION_VIEW_JSON_KEY) + " on entity " + targetCls);
+			}
+		}
+		else {
+			firstLinMap.remove(LINEARIZATION_PARENT_JSON_KEY);
+			if (linParent1 != null ||linParent2 != null) {
+				log.warn("Ignoring linerization parents (" + linParent1 + "/" + linParent2 + ") for TELESCOPIC linearization view " +
+						firstLinMap.get(LINEARIZATION_VIEW_JSON_KEY) + " on entity " + targetCls);
+			}
+		}
+
+		String notes1 = firstLinMap.get(CODING_NOTE_JSON_KEY);
+		String notes2 = secondLinMap.get(CODING_NOTE_JSON_KEY);
+//		if (! isTelescopic) {
+			if (notes1 == null && notes2 != null) {
+				firstLinMap.put(CODING_NOTE_JSON_KEY, notes2);
+			}
+			else if (notes2 == null) {
+				//do nothing (i.e. keep notes1)
+			}
+			else if (! notes1.equals(notes2)) {
+				log.warn("Conflicting values (" + notes1 + "/" + notes2 + ") for " + CODING_NOTE_JSON_KEY + " for linearization view " +
+						firstLinMap.get(CODING_NOTE_JSON_KEY) + " on entity " + targetCls);
+			}
+//		}
+//		else {
+//		
+//		}
+
 	}
 
 	private Map<String, String> exportResiduals(OWLClass targetCls) {
@@ -92,7 +218,7 @@ public class LinearziationExporter {
 
 		Map<String, String> linMap = new HashMap<String, String>();
 
-		linMap.put("linearizationView", newLinViewName);
+		linMap.put(LINEARIZATION_VIEW_JSON_KEY, newLinViewName);
 
 		RDFResource linParent = (RDFResource) linSpec.getPropertyValue(cm.getLinearizationParentProperty());
 
@@ -100,7 +226,7 @@ public class LinearziationExporter {
 			String parentPublicId = PublicIdCache.getPublicId(cm,
 					cm.getOwlModel().getRDFSNamedClass(linParent.getName()));
 			if (parentPublicId != null) {
-				linMap.put("linearizationParent", parentPublicId);
+				linMap.put(LINEARIZATION_PARENT_JSON_KEY, parentPublicId);
 			} else {
 				log.warn("Could not find public id for JSON export for: " + linParent.getName()
 						+ " used as a linearization parent for class: " + sourceCls.getName());
@@ -112,13 +238,13 @@ public class LinearziationExporter {
 		if (codingNoteTerm != null) {
 			String codingNote = (String) codingNoteTerm.getPropertyValue(cm.getLabelProperty());
 			if (codingNote != null) {
-				linMap.put("codingNote", codingNote);
+				linMap.put(CODING_NOTE_JSON_KEY, codingNote);
 			}
 		}
 
-		exportBooleanLinProp(linSpec, cm.getIsIncludedInLinearizationProperty(), "isIncludedInLinearization", false, linMap);
-		exportBooleanLinProp(linSpec, cm.getIsGroupingProperty(), "isGrouping", false, linMap);
-		exportBooleanLinProp(linSpec, cm.getIsAuxiliaryAxisChildProperty(), "isAuxiliaryAxisChild", false, linMap);
+		exportBooleanLinProp(linSpec, cm.getIsIncludedInLinearizationProperty(), IS_INCLUDED_IN_LINEARIZATION_JSON_KEY, false, linMap);
+		exportBooleanLinProp(linSpec, cm.getIsGroupingProperty(), IS_GROUPING_JSON_KEY, false, linMap);
+		exportBooleanLinProp(linSpec, cm.getIsAuxiliaryAxisChildProperty(), IS_AUXILIARY_AXIS_CHILD_JSON_KEY, false, linMap);
 
 		return linMap;
 	}
@@ -132,7 +258,7 @@ public class LinearziationExporter {
 			bool = Boolean.FALSE;
 		}
 		
-		String value = bool == null ? "unknown" : bool.toString();
+		String value = bool == null ? UNKNOWN : bool.toString();
 		
 		map.put(jsonKey, value);
 	}
